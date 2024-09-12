b976422d4423e5fd57c5365902eb50c974c4822f
Serena <spuertas@uade.edu.ar>
2024-09-12 09:27:49 -0300

    v1

---
+++ TP.2/ejercicio_8.py
@@ -0,0 +1,9 @@
+"""Ejercicio 7"""
+
+impares = [x for x in range(100, 201) if x % 2 != 0]
+"""
+    -x for x in: Esta es la lista por compresion
+    -range(100, 201): Secuencia de numeros de 100 a 200 inclusive
+    -if x % 2 !0: Filtra todos los numeros impares, es decir; los que no son divisibles por 2
+"""
+print(impares)

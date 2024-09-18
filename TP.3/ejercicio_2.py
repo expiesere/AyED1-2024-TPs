e5230ddc8847830c6961ee17db75e6d353dfbc23
Serena <spuertas@uade.edu.ar>
2024-09-18 18:42:08 -0300

    v1 :D

---
+++ TP.3/ejercicio_2.py
@@ -0,0 +1,35 @@
+"""Ejercicio 2"""
+
+import random as rn
+
+
+def cargar_matriz(n: int) -> list:
+    """
+    Carga una matriz de tamaño NxN con numeros al azar.
+
+    Pre:
+        -n (int): El numero de filas y columnas que tiene la matriz.
+
+    Post:
+        -X[NxN[int]]: Matrices con numeros al azar.
+    """
+    return [[rn.randint(1, 100) for _ in range(n)] for _ in range(n)]
+
+
+def main() -> None:
+    """
+    Le solicita al usuario una cantidad de matrices y sus tamaños,
+    luego invoca la funcion cargar_matriz para rellenarlas.
+    """
+    x = int(input("Cantidad de matrices a generar: "))
+    n = int(input("Tamaño de matrices: "))
+    for i in range(x):
+        matriz = cargar_matriz(n)
+        print(f"\nMATRIZ N°{i+1}")
+        for fila in matriz:
+            print(" ".join(map(str, fila)))
+        print()
+
+
+if __name__ == "__main__":
+    main()
